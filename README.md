2cc2daba7da07356159de3afe7704318976a561b
nathanllee1 <nathan.l.lee123@gmail.com>
2022-10-08 18:23:39 -0700

    Made instruction text more clear

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Tabify
 
 Pull tabs from Ultimate Guitar for the current Spotify song playing
 
+## Environment Setup
+Go to the [spotify developer dashboard](https://developer.spotify.com/dashboard/) and create a new app.  
+Create a `.env` file with 2 variables.
+```
+SPOTIFY_CLIENT_ID='YOUR_CLIENT_ID'
+SPOTIFY_CLIENT_SECRET='YOUR_CLIENT_SERE'
+```
 ```
 npm install
 npm run run-dev
 ```
+This spins up a vite server for the frontend and starts up the serverless functions in the backend!
--- src/Splash.svelte
+++ src/Splash.svelte
@@ -3,19 +3,17 @@
 </script>
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col flex-wrap lg:flex-row-reverse gap-14">
-        <div class="shadow-lg rounded-lg p-12 dark:bg-slate-800 bg-white">
+        <div class="shadow-lg rounded-lg p-6 dark:bg-slate-800 bg-white">
             <h1 class="text-5xl">Tabify</h1>
             <div class="py-6">
-                Ever be jamming to a song on Spotify and have to Google the chords?
+                Do you ever find yourself jamming to a song on Spotify, and want to know the chords?
             </div>
             <div>
-                Tabify connects to Spotify and automatically pulls up chords from Ultimate Guitar.
+                Login to Spotify, play your song, and Tabify will automically pull up the chord sheet!
             </div>
             <br/>
-            <img src="/app.png" alt="app diagram"/>
-            <!--
-            <object id ="animated-svg" type="image/svg+xml" data="./animation.svg" title="app demo"></object>
-            -->
+            <img src="/AppLight.png" alt="app diagram" />
+            <br />
             <a class="btn" href="/api/login"> Login to Spotify </a>
 
         </div>

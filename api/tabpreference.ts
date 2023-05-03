d22bad2ee05b81832f47965957592c3836200c04
nathanllee1 <nathan.l.lee123@gmail.com>
2023-05-02 21:19:24 -0700

    Messy -ish fix

--- api/tabpreference.ts
+++ api/tabpreference.ts
@@ -1,6 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { Connection, createConnection } from "mysql2";
-import { convertToMySQLDate, getUserFromSession, query } from "./utils";
 
 export default async function handler(
   request: VercelRequest,
@@ -58,3 +57,38 @@ export default async function handler(
     return;
   }
 }
+
+export const query = (
+  connection: Connection,
+  query: string,
+  subs: any[],
+) => {
+  return new Promise((accept, reject) => {
+    connection.query(
+      { sql: query },
+      subs,
+      function (err, results, fields) {
+        if (err) {
+          reject(err);
+        }
+        accept(results);
+      },
+    );
+  });
+};
+export const getUserFromSession = async (
+  connection: Connection,
+  session: string,
+) => {
+  return await query(
+    connection,
+"SELECT DISTINCT USER_ID \
+    FROM SESSIONS \
+    WHERE SESSION_ID = ?",
+    [session],
+  );
+};
+
+export const convertToMySQLDate = (date: Date) => {
+  return date.toISOString().slice(0, 19).replace("T", " ");
+};
